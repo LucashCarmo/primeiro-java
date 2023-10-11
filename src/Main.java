45e340b38762467a4f6f80b40af5fa16273a60a3
Lucas Carmo <gamerlucas96@gmail.com>
2023-10-10 23:18:22 -0300

    melhorando o codigo

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String nome = "Lucas henrique";
+        String tipoConta = "Corrente";
         double saldo = 2500.00;
         int operador = 0;
 
@@ -11,19 +13,22 @@ public class Main {
         System.out.println("***********************************************");
         System.out.println("Dados inicias do cliente: \n");
 
-        System.out.println("Nome:             Lucas Henrique");
-        System.out.println("Tipo conta:       Corrente");
-        System.out.println("Saldo Inicial:    R$ 2500,00");
+        System.out.println("Nome:            " + nome);
+        System.out.println("Tipo conta:      " + tipoConta);
+        System.out.println("Saldo Inicial:    R$ " + saldo);
         System.out.println("*********************************************** \n\n");
+
+        String menu = """
+                    Operaçôes
+                    1- Consultar saldos
+                    2- Receber valor
+                    3- Transferir valor
+                    4- Sair
+                """;
 
         while (operador != 4) {
 
-            System.out.println("\n Operaçôes \n");
-            System.out.println("1- Consultar saldos");
-            System.out.println("2- Receber valor");
-            System.out.println("3- Transferir valor");
-            System.out.println("4- Sair \n");
-
+            System.out.println(menu);
             System.out.println("Digite a opção desejada: ");
             operador = scanner.nextInt();
 
@@ -36,7 +41,11 @@ public class Main {
             } else if (operador == 3) {
                 System.out.println("Informe o valor que deseja transferir: ");
                 double transferir = scanner.nextDouble();
-                System.out.println("Saldo atualizado: " + (saldo= saldo - transferir));
+                if (transferir > saldo){
+                    System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                } else {
+                    System.out.println("Saldo atualizado: " + (saldo= saldo - transferir));
+                }
             } else {
                 break;
             }
